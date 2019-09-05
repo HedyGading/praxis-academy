9b98befa85e43704f7ce4e8fbea31a9aec291c74
HedyGading <hedygading1@gmail.com>
2019-09-05 02:36:32 -0700

    Latihan poin 3

---
+++ Minggu 1/04/Latihan/Latihan04_3/Deposit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kasus04;
+
+public class Deposit extends ATMMachine
+{
+    static double deposit;
+    public void setDeposit(double d)
+    {
+        deposit = d;
+    }
+    public static double getDeposit()
+    {
+        return deposit;
+    }
+}
