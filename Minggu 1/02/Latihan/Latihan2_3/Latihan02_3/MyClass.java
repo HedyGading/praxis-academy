a7def4db88e42be54cf59d16d5f5cb7521857912
HedyGading <hedygading1@gmail.com>
2019-09-10 20:42:51 -0700

    push

---
+++ Minggu 1/02/Latihan/Latihan2_3/Latihan02_3/MyClass.java
@@ -0,0 +1,11 @@
+// Name of the package must be same as the directory
+// under which this file is saved
+package Latihan02_3;
+
+public class MyClass
+{
+    public void getNames(String s)
+    {        
+        System.out.println(s);  
+    }
+}
