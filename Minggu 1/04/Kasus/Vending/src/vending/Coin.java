a56ad0e004521b3a0f715c202a4537a85fd0966e
HedyGading <hedygading1@gmail.com>
2019-10-01 00:57:50 -0700

    push

---
+++ Minggu 1/04/Kasus/Vending/src/vending/Coin.java
@@ -0,0 +1,7 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vending;
+public enum Coin { PENNY(1), NICKLE(5), DIME(10), QUARTER(25); private int denomination; private Coin(int denomination){ this.denomination = denomination; } public int getDenomination(){ return denomination; } }
---
+++ Minggu 1/04/Kasus/Vending/src/vending/NotSufficientChangeException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vending;
+
+public class NotSufficientChangeException extends RuntimeException {
+    private String message;
+   
+    public NotSufficientChangeException(String string) {
+        this.message = string;
+    }
+   
+    @Override
+    public String getMessage(){
+        return message;
+    }
+   
+}
